cf70618c6634f12d0c5270e1bceb9059b7061f4a
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2024-06-08 18:51:45 +0530

    Updated Solution - GfG to GitHub

--- Basic/Prime Number/prime-number.java
+++ Basic/Prime Number/prime-number.java
@@ -20,19 +20,17 @@ class GFG
 
 
 class Solution{
-    static int isPrime(int N){
+    static int isPrime(int n){
         // code here
-        
-        if(N==0 || N==1)
-        {
-            return 0;
-        }
-        for(int i=2; i<=Math.sqrt(N);i++)
-        {
-            if(N%i==0)
+        if(n==0 || n==1){
             return 0;//not prime
-            
         }
-        return 1;//prime
+        
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i == 0){
+                return 0;//not prime
+            }
+        }
+        return 1;//prime number
     }
 }
